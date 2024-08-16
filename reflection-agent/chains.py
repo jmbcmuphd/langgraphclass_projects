c74dd02a2c8b3690a74455cc96407d2d02727b8b
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-16 08:58:25 -0400

    Generated graph and printed it using mermaid and in ascii

--- reflection-agent/chains.py
+++ reflection-agent/chains.py
@@ -1,24 +1,27 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 
-reflection_prompt = ChatPromptTemplate.from_message(
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reflection_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            """You are a viral twitter influencer grading a tweet. Generate critique and recommendations 
-           for the user's tweet""",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
             "Always provide detailed recommendations, including requests for length, virality, style, etc.",
         ),
         MessagesPlaceholder(variable_name="messages"),
     ]
 )
 
-generation_prompt = ChatPromptTemplate.from_message(
+generation_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts.",
-            " Generate the best twitter post possible for the user's request.",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the best twitter post possible for the user's request."
             " If the user provides critique, respond with a revised version of your previous attempts.",
         ),
         MessagesPlaceholder(variable_name="messages"),
