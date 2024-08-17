461a0eb5646e1e482ef9c1a84169b5dbeab19193
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-17 07:50:24 -0400

    Created execute_tools() function that calls tavily search and provides the output of the search.

---
+++ reflexion-agent/tool_executor.py
@@ -0,0 +1,71 @@
+from typing import List
+
+from dotenv import load_dotenv
+from langchain_community.tools import TavilySearchResults
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_core.messages import BaseMessage, ToolMessage, HumanMessage, AIMessage
+from langgraph.prebuilt import ToolInvocation, ToolExecutor
+
+from chains import parser
+from schemas import AnswerQuestion, Reflection
+
+load_dotenv()
+
+
+search = TavilySearchAPIWrapper()
+tavily_tool = TavilySearchResults(api_wrapper=search, max_results=5)
+tool_executor = ToolExecutor([tavily_tool])
+
+
+def execute_tools(state: List[BaseMessage]) -> List[ToolMessage]:
+    tool_invocation: AIMessage = state[-1]
+    parsed_tool_calls = parser.invoke(tool_invocation)
+
+    ids = []
+    tool_invocations = []
+
+    for parsed_call in parsed_tool_calls:
+        for query in parsed_call["args"]["search_queries"]:
+            tool_invocations.append(
+                ToolInvocation(tool="tavily_search_results_json", tool_input=query)
+            )
+            ids.append(parsed_call["id"])
+
+    outputs = tool_executor.batch(tool_invocations)
+    pass
+
+
+if __name__ == "__main__":
+    print("Tool Executor Enter")
+    human_message = HumanMessage(
+        content="Write about AI-Powered SOC / autonomous soc problem domain,"
+        " list startups that do that and raised capital."
+    )
+    answer = AnswerQuestion(
+        answer="",
+        reflection=Reflection(missing="", superfluous=""),
+        search_queries=[
+            "AI-powered SOC startups funding",
+            "AI SOC problem domain specifics",
+            "Technologies used by AI-powered SOC startups",
+        ],
+        id="call_somecallidsetbylangchain",
+    )
+
+    raw_res = execute_tools(
+        state=[
+            human_message,
+            AIMessage(
+                content="",
+                tool_calls=[
+                    {
+                        "name": AnswerQuestion.__name__,
+                        "args": answer.dict(),
+                        "id": "call_somecallidsetbylangchain",
+                    }
+                ],
+            ),
+        ]
+    )
+
+    print(raw_res)
