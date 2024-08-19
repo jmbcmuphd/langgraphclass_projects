72917a210bfa4f814f3a34c02dc80e3480ac95b2
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-19 19:32:59 -0400

    Created graph with four nodes, successfully implemented advanced RAG using langgraph

--- langgraph-course-crag/graph/chains/generation.py
+++ langgraph-course-crag/graph/chains/generation.py
@@ -2,6 +2,7 @@ from langchain import hub
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
