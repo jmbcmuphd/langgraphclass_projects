3c2f661d307e3f86bb4730f9226e1f85d04b7917
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-20 19:08:06 -0400

    added sqlite capability

--- human-in-the-loop-memory/main.py
+++ human-in-the-loop-memory/main.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-load_dotenv()
-
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+load_dotenv()
 
 
 class State(TypedDict):
@@ -32,20 +33,25 @@ builder.add_edge("step_1", "human_feedback")
 builder.add_edge("human_feedback", "step_3")
 builder.add_edge("step_3", END)
 
-memory = MemorySaver()
+
+def memory_gen():
+    with SqliteSaver.from_conn_string("checkpoints.sqlite") as memory:
+        yield memory
+
+
+memory = next(memory_gen())
 
 graph = builder.compile(checkpointer=memory, interrupt_before=["human_feedback"])
 graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
-
 if __name__ == "__main__":
-    thread = {"configurable": {"thread_id": "1"}}
-    initial_input = {"input": "hello world"}
-
-    for event in graph.stream(initial_input, thread, stream_mode="values"):
-        print(event)
-
-    print(graph.get_state(thread).next)
+    thread = {"configurable": {"thread_id": "7777"}}
+    # initial_input = {"input": "hello world"}
+    #
+    # for event in graph.stream(initial_input, thread, stream_mode="values"):
+    #     print(event)
+    #
+    # print(graph.get_state(thread).next)
 
     user_input = input("Tell me how you want to update the state: ")
     graph.update_state(thread, {"user_feedback": user_input}, as_node="human_feedback")
