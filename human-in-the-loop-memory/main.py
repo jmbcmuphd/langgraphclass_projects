f0108cf5f22d14d35319871d6f82fc457265f449
jmbcmuphd <163065508+jmbcmuphd@users.noreply.github.com>
2024-08-20 18:39:19 -0400

    added load dotenv

--- human-in-the-loop-memory/main.py
+++ human-in-the-loop-memory/main.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
